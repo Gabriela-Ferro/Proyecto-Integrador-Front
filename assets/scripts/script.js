54e76dfd0709a92d3933fd9a6ba05f163064c386
Sebastian Montes de Oca <ing.montesdeoca94@gmail.com>
2023-11-26 22:19:50 -0600

    add: foto tarjeta Sebas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,7 +16,7 @@ class MemberCard {
 	createCard() {
 		return `
             <article class="card-team w-fit">
-                <div class="mt-4 card-team-container">
+                <div class="mt-4 card-team-container ">
                     <figure class="card-team-figure">
                         <img
                             src="${this.#img}"
@@ -89,7 +89,7 @@ let cardArely = new MemberCard(
 let cardSebastian = new MemberCard(
 	"Sebastian Montes de Oca",
 	"Developer",
-	"./assets/img/.png",
+	"./assets/img/sebas.png",
 	"https://github.com/sebastianmov",
 	"Un placer, mi nombre es Sebastian Montes de Oca, Ingeniero de minas y metalurgia. Actualmente me encuentro en preparación de ser Developer Jr. Soy geek, disfruto aprender sobre programación y se que siendo tolerante, enfocado y perseverante lograre analizar los problemas y resolverlos de manera correcta. Me encanta la música, bailar y rodar en bicicleta. Lo mejor que existe para mí es observar el verde de los caminos que atraviesas."
 );
@@ -99,7 +99,7 @@ let cardGabriela = new MemberCard (
 	"Product Owner",
 	"./assets/img/FotoGabyFerroRz.png",
 	"https://github.com/Gabriela-Ferro",
-	"¡Hola! Soy Gabriela, actualmente fungo como Product Owner y miembro del Development Team de este gran proyecto. Estudie Derecho en la UNAM y actualmente me formo como desarrolladora junior. Tengo multiples intereses y pasatiempos muy diversos, en especial me gustan las artes visuales, la naturaleza y los deportes. Me interesa el diseño UX-UI, y mi meta es trabajar diseñando y programando páginas accesibles con visuales atractivos que impacten positivamente a las personas.!"
+	"¡Hola! Soy Gabriela, actualmente funjo como Product Owner y miembro del Development Team de este gran proyecto. Estudie Derecho en la UNAM y actualmente me formo como desarrolladora junior. Tengo multiples intereses y pasatiempos muy diversos, en especial me gustan las artes visuales, la naturaleza y los deportes. Me interesa el diseño UX-UI, y mi meta es trabajar diseñando y programando páginas accesibles con visuales atractivos que impacten positivamente a las personas.!"
 
 
 );
