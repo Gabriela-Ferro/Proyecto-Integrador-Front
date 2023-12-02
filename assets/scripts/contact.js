eb1437d06c4fca980649103f835d2182d3578913
Andres Perez <andresperez2098@gmail.com>
2023-12-02 03:24:33 -0600

    Added: Validating function for email field with regex

--- assets/scripts/contact.js
+++ assets/scripts/contact.js
@@ -1,17 +1,17 @@
 
-const contactForm = document.forms["contactForm"];
+const contactForm = document.forms["contact-form"];
 
 
 contactForm.addEventListener( "submit" , ( event )=>{
     event.preventDefault();
-    // console.log(event);
+     console.log(event);
 
      const user = {
          name : contactForm.elements["contact-form-name"].value ,
          email : contactForm.elements["contact-form-email"].value ,
-         number : contactForm.elements["contact-form-number"].value,
-         issue : contactForm.options["contactMessage"].value,
-         message : contactForm.elements["contactMessage"].value,
+         phone : contactForm.elements["contact-form-phone"].value,
+         issue : contactForm.elements["contact-form-issue"].value,
+         message : contactForm.elements["contact-form-message"].value,
      }
 
     if( datosVerificados( user )  ){
@@ -21,31 +21,36 @@ contactForm.addEventListener( "submit" , ( event )=>{
 } );
 
 const datosVerificados = ( user ) => {
-    let response = true;
 
-    if( user.email === "" ){
-        mensajeError("Falta introducir el email");
+    let response = true;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if (user.email.trim() || emailRegex.test(user.email)) {
+        mensajeErrorEmail('Por favor, ingrese un correo electrónico válido.');
         response = false;
-    } else if( user.password.length < 8) {
-        mensajeError("El password debe ser mayor o igual a 8 caracteres");
-        response = false;
-    } else if( user.password !== user.confirmPassword) {
-        mensajeError("La confirmación de contraseña no coincide");
-        response = false;
-    } else if ( !user.termsandconditions ) {
-        mensajeError("Debe aceptar los términos y condiciones");
-        response = false;
-    } else {
-        mensajeError("");
     }
 
     return response;
 }
 
-const mensajeError = ( message ) =>{
+const mensajeErrorEmail = ( message ) =>{
     console.log( message);
-    const errorMessage = document.getElementById("error-message");
-    const errorMessageContainer = document.getElementById("error-message-container");
+    const errorMessage = document.getElementsById("invalid-email-message");
+     const errorMessageContainer = document.getElementById("invalid-email-container");
+
+    errorMessage.innerHTML = message;
+    if( message === "" ){
+        // displaty: none: quita el elemento del DOM
+        // visibility: hidden : ocultar visualmente el elemento
+        errorMessageContainer.style.display = "none";
+    } else{
+        errorMessageContainer.style.display = "block";
+    }
+}
+const mensajeErrorTel = ( message ) =>{
+    console.log( message);
+    const errorMessage = document.getElementsById("invalid-phone-message");
+     const errorMessageContainer = document.getElementById("invalid-phone-container");
 
     errorMessage.innerHTML = message;
     if( message === "" ){
@@ -61,4 +66,4 @@ const enviarDatosAlServidor = ( user ) => {
     console.table( user );
     // TODO enviar datos a la api del servidor
 }
-console.log(user);
+// console.log(user);
