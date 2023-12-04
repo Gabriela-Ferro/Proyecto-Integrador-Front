919ddf5f6127064d1f932c97006b112fc244c887
Andres Perez <andresperez2098@gmail.com>
2023-12-03 20:26:59 -0600

    Email link added

---
+++ assets/scripts/prueba.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('submit-button');
+
+document.getElementById('contact-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_ubhucbb';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
